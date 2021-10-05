3350912c7188dec79c29df41a59188f16ae78368
Dmitriy Usatov <usatov.dv@gmail.com>
2021-10-05 15:51:21 +0300

    added optional dialog

---
+++ lib/components/dialogs/am_optional_dialog.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class OptionalResult {
+  final bool generalResult;
+  final Map<String, bool> options;
+
+  OptionalResult({this.generalResult, this.options});
+}
+
+class AMOptionalDialog extends StatefulWidget {
+  final String title;
+  final String description;
+  final Map<String, bool> options;
+  final String actionText;
+  final String cancelText;
+  final List<Widget> actions;
+
+  const AMOptionalDialog({
+    Key key,
+    this.title,
+    this.description,
+    @required this.options,
+    @required this.actionText,
+    @required this.cancelText,
+    this.actions,
+  }) : super(key: key);
+
+  static Future<bool> show(
+    BuildContext context,
+    String title,
+    String description,
+    Map<String, bool> options,
+    String actionText,
+    String cancelText, {
+    List<Widget> actions,
+  }) {
+    return showCupertinoDialog(
+        context: context,
+        builder: (_) => AMOptionalDialog(
+              title: title,
+              description: description,
+              options: options,
+              actionText: actionText,
+              cancelText: cancelText,
+              actions: actions,
+            )).then((value) => (value as bool) ?? false);
+  }
+
+  @override
+  _OptionalDialogState createState() => _OptionalDialogState();
+}
+
+class _OptionalDialogState extends State<AMOptionalDialog> {
+  String title;
+  String description;
+  Map<String, bool> options;
+  String actionText;
+  String cancelText;
+  List<Widget> actions;
+
+  @override
+  void initState() {
+    super.initState();
+    title = widget.title;
+    description = widget.description;
+    options = widget.options;
+    actionText = widget.actionText;
+    cancelText = widget.cancelText;
+    actions = widget.actions;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> children = [
+      if (description != null)
+        Padding(
+          padding: const EdgeInsets.only(bottom: 16),
+          child: Text(description),
+        )
+    ];
+    for (final key in options.keys) {
+      final row = GestureDetector(
+        onTap: () => _onTapOption(key),
+        child: Row(
+          children: [
+            Checkbox(
+              value: options[key],
+              onChanged: (_) => _onTapOption(key),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(right: 16),
+                child: Text(key),
+              ),
+            ),
+          ],
+        ),
+      );
+
+      children.add(row);
+    }
+    return AlertDialog(
+      title: title == null ? null : Text(title),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: children,
+      ),
+      actions: <Widget>[
+        FlatButton(
+          child: Text(cancelText ?? "Cancel"),
+          onPressed: () => Navigator.pop(
+            context,
+            OptionalResult(generalResult: false, options: options),
+          ),
+        ),
+        if (actions != null) ...actions,
+        FlatButton(
+          child: Text(actionText ?? "OK"),
+          onPressed: () => Navigator.pop(
+            context,
+            OptionalResult(generalResult: true, options: options),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _onTapOption(String key) {
+    setState(() {
+      options[key] = !options[key];
+    });
+  }
+}
